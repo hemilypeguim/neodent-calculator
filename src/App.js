ac55932d9e54973e7888cad13e0af8f7eea50333
hemilypeguim <hemily_peguimm@outlook.com>
2024-06-25 22:30:24 -0300

    first load

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className='header-container'>
+        <img className='logo' src="logo-neodent.png" alt="logo" />
+        <button className='btn'>KPI</button>
+      </div>
+      <div className='main-container'>
+        <div className='input-fields'>
+          <div className='input-field'>
+            <label>Valor inicial da proposta:</label>
+            <input className='calc-input' placeholder='Digite aqui' type='text' />
+          </div>
+          <div className='input-field'>
+            <label>Valor final da proposta:</label>
+            <input className='calc-input' placeholder='Digite aqui' type='text' />
+          </div>
+          <div className='input-field'>
+            <label>Prazo inicial:</label>
+            <input className='calc-input' placeholder='Digite aqui' type='text' />
+          </div>
+          <div className='input-field'>
+            <label>Valor do desconto à vista:</label>
+            <input className='calc-input' placeholder='Digite aqui' type='text' />
+          </div>
+          <button className='clean-btn'>Limpar</button>
+        </div>
+        <div className='result-fields'>
+          <div className='result-name'>
+            <p>Resultados</p>
+            <div className='underline'></div>
+            <div className='result-input-fields'>
+              <div className='input-field'>
+                <label>Custo risco sacado:</label>
+                <input className='result-input' placeholder='$' type='text' />
+              </div>
+              <div className='input-field'>
+                <label>Valor para 70 dias:</label>
+                <input className='result-input' placeholder='$' type='text' />
+              </div>
+              <div className='input-field'>
+                <label>Desconto real:</label>
+                <input className='result-input' placeholder='$' type='text' />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
